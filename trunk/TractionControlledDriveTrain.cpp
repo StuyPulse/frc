430ad396e0cda7a799b96e8f771f65b44f057a4f
Jordan <@>
2009-01-06 09:26:34 +0000

    added more description to traction controlled drive train

--- trunk/TractionControlledDriveTrain.cpp
+++ trunk/TractionControlledDriveTrain.cpp
@@ -1,5 +1,9 @@
 // Extends BaseDriveTrain
 
-// Has an encoder and accelerometer? instance variable
 
-// Overrides setSpeed() so that a speed change request is not just passed through to the SpeedControllers. Instead, this setSpeed method invkes a routine which "fades" the speed controller up to this speed over time using the accelerometer and encoders. It can also just use time for now.
+// Has weight and wheel_cof instance variables
+
+
+// methods to change wheel_cof and wheel_weight
+
+// Overrides setSpeed() so that a speed change request is not just passed through to the SpeedControllers. Instead, this setSpeed method invkes a routine which "fades" the speed controller up to this speed over time based on the max acceleration as determined by the wheel_cof and wheel_weight instance variables.
