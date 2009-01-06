0bc845307ede107e575be40109cd0bf8569e9fda
Jordan <@>
2009-01-06 08:34:39 +0000

    Removing WPILib, use the one in Windriver

--- trunk/WPILib/CInterfaces/CDigitalInput.h
+++
@@ -1,8 +0,0 @@
-#ifndef _C_DIGITAL_INPUT_H
-#define _C_DIGITIL_INPUT_H
-
-UINT32 GetDigitalInput(UINT32 slot, UINT32 channel);
-UINT32 GetDigitalInput(UINT32 channel);
-
-#endif
-
--- trunk/WPILib/CInterfaces/CWrappers.h
+++
@@ -1,8 +0,0 @@
-#ifndef C_WRAPPERS_H
-#define C_WRAPPERS_H
-
-class SensorBase;
-typedef SensorBase *(*SensorCreator)(UINT32 slot, UINT32 channel);
-
-#endif
-
