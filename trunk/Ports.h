a6bd755e7f9f66178b9f3cd297cc9b370486dc11
Jordan <@>
2009-02-01 04:49:07 +0000

    formatted ports.h better

--- trunk/Ports.h
+++ trunk/Ports.h
@@ -1,23 +1,31 @@
-// Michael1.h
-
 //Driver Station
+
 #define LEFT_DRIVE_JOYSTICK 1
 #define RIGHT_DRIVE_JOYSTICK 2
 #define SHOOTER_JOYSTICK 3
 
+
+
 //Digital Sidecar PWM (slot 4)
+
 #define LEFT_DRIVE_MOTOR 1
 #define RIGHT_DRIVE_MOTOR 2
 #define INTAKE_ROLLER 3
 #define SHOOTER_ROLLER 4
 
+
+
 //Digital Sidecar DigIO (slot 4)
+
 #define ARIELS_LIGHT 1
 #define LEFT_ENCODER_A 2
 #define LEFT_ENCODER_B 3
 #define RIGHT_ENCODER_A 4
 #define RIGHT_ENCODER_B 5
 
+
+
 //Analog Sensors
+
 #define ACCELEROMETER 1
 #define GYRO 2
