ec618c8e75e2580a1f5068e8102819f403528234
Alejandro Carrillo <aecarrillo594@gmail.com>
2011-01-18 22:08:00 -0500

    Fixed master branch.

--- desdroid/src/stuy/Autonomous.java
+++ desdroid/src/stuy/Autonomous.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.*;
 
 public class Autonomous {
     DESdroid des;
-    
+
     DigitalInput left; // digital inputs for line tracking sensors
     DigitalInput middle;
     DigitalInput right;
@@ -23,8 +23,6 @@ public class Autonomous {
     Encoder encoderRight;
 
     Gyro gyro;
-
-    int length = 72;
 
     public Autonomous(DESdroid d) {
         des = d;
@@ -43,27 +41,6 @@ public class Autonomous {
     }
 
     public void lineTrackStraight() {
-        des.dt.mecanumDrive_Cartesian(0.0, 0.7, 0.0, gyro.getAngle());
-
-        while (getAvgDistance() < length) {
-            if (left.get() && !middle.get()) {
-                des.dt.mecanumDrive_Cartesian(0.1, 0.7, 0.0, gyro.getAngle());
-            } else if (right.get() && !middle.get()) {
-                des.dt.mecanumDrive_Cartesian(-0.1, 0.7, 0.0, gyro.getAngle());
-            } else if (right.get() && middle.get() && right.get()) {
-                break;
-            }
-
-            des.dt.mecanumDrive_Cartesian(0.0, 0.0, 0.0, 0.0);
-
-
-        }
-
-    }
-
-    public double getAvgDistance() {
-
-        return (encoderLeft.getDistance() + encoderRight.getDistance()) / 2.0;
 
     }
 
