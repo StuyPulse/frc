94914f00e7b8f5a64d3e54f5839d64ab6d52da2a
Blake Elias <blakeelias@gmail.com>
2011-01-24 16:35:23 -0500

    Eliminated encoders from Autonomous (would not work)

--- desdroid/src/stuy/Autonomous.java
+++ desdroid/src/stuy/Autonomous.java
@@ -19,9 +19,6 @@ public class Autonomous {
     DigitalInput middle;
     DigitalInput right;
 
-    Encoder encoderLeft;
-    Encoder encoderRight;
-
     Gyro gyro;
 
     public Autonomous(DESdroid d) {
@@ -30,12 +27,6 @@ public class Autonomous {
         left = new DigitalInput(1);
         middle = new DigitalInput(2);
         right = new DigitalInput(3);
-
-        encoderLeft = new Encoder(3, 4, true);
-        encoderRight = new Encoder(5, 6, true);
-
-        encoderLeft.start();
-        encoderRight.start();
 
         gyro = new Gyro(0);
     }
