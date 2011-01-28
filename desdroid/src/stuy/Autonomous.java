0400b3f265b884e5ac1e61b73baf085a3e2115f7
Blake Elias <blakeelias@gmail.com>
2011-01-27 22:14:40 -0500

    Integrated working LineTracker code into the desdroid project.

--- desdroid/src/stuy/Autonomous.java
+++ desdroid/src/stuy/Autonomous.java
@@ -3,83 +3,54 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Alex Carrillo
  */
-
 package stuy;
+
 import edu.wpi.first.wpilibj.*;
 
-public class Autonomous {
+public class Autonomous implements Constants {
+
     DESdroid des;
-
-    DigitalInput left; // digital inputs for line tracking sensors
-    DigitalInput middle;
-    DigitalInput right;
-
-    Gyro gyro;
-
-    OperatorInterface oi;
+    int leftValue, middleValue, rightValue;
 
     public Autonomous(DESdroid d) {
         des = d;
-
-        left = new DigitalInput(1);
-        middle = new DigitalInput(2);
-        right = new DigitalInput(3);
-
-        // set the MotorSafety expiration timer
-        des.drive.setExpiration(15);
-
-
-        gyro = new Gyro(0);
-
-        oi = new OperatorInterface();
     }
 
-    public void lineTrackStraight() {
+    public void getSensorValues() {
+        leftValue = des.leftSensor.get() ? 1 : 0;
+        middleValue = des.middleSensor.get() ? 1 : 0;
+        rightValue = des.rightSensor.get() ? 1 : 0;
+    }
+
+    public void printLineStatus() {
+        System.out.println("L: [" + (leftValue == 1 ? "1" : " ") + "] "
+                + "M: [" + (middleValue == 1 ? "1" : " ") + "] "
+                + "R: [" + (rightValue == 1 ? "1" : " ") + "]");
+    }
+
+    public int binaryValue(boolean goLeft) {
+        if (goLeft)
+            return leftValue * 4 + middleValue * 2 + rightValue;
+        return rightValue * 4 + middleValue * 2 + leftValue;
+    }
+
+    public void lineTrack(boolean straightLine, boolean goLeft) {
 
         int binaryValue; // a single binary value of the three line tracking
-                        // sensors
+        // sensors
         int previousValue = 0; // the binary value from the previous loop
         double steeringGain; // the amount of steering correction to apply
 
         // the power profiles for the straight and forked robot path. They are
         // different to let the robot drive more slowly as the robot approaches
         // the fork on the forked line case.
-
-
-        double forkProfile[] = {0.70, 0.70, 0.55, 0.60, 0.60, 0.50, 0.40, 0.00};
-        double straightProfile[] = {0.7, 0.7, 0.6, 0.6, 0.35, 0.35, 0.35, 0.0};
-
         double powerProfile[];   // the selected power profile
-
-        // set the straightLine and left-right variables depending on chosen path
-        boolean straightLine;
-        boolean goLeft;
-        switch (oi.getAutonSwitch()) {
-            case 1: // Go straight
-                straightLine = true;
-                goLeft = false;
-            case 2: // Go left
-                straightLine = false;
-                goLeft = true;
-            case 3: // Go right
-                straightLine = false;
-                goLeft = false;
-            default:
-                straightLine = true;
-                goLeft = false;
-        }
-//        straightLine = des.ds.getDigitalIn(des.DIGITAL_IN_STRAIGHT_LINE);
-        powerProfile = (straightLine) ? straightProfile : forkProfile;
+        powerProfile = (straightLine) ? STRAIGHT_PROFILE : FORK_PROFILE;
         double stopTime = (straightLine) ? 2.0 : 4.0; // when the robot should look for end
-//        goLeft = !des.ds.getDigitalIn(des.DIGITAL_IN_GO_LEFT) && !straightLine;
-        System.out.println("StraightLine: " + straightLine);
-        System.out.println("GoingLeft: " + goLeft);
-
 
         boolean atCross = false; // if robot has arrived at end
 
@@ -88,27 +59,15 @@ public class Autonomous {
         timer.start();
         timer.reset();
 
-        int oldTimeInSeconds = -1;
         double time;
         double speed, turn;
 
         // loop until robot reaches "T" at end or 8 seconds has past
         while ((time = timer.get()) < 8.0 && !atCross) {
             int timeInSeconds = (int) time;
-            // read the sensors
-            int leftValue = left.get() ? 1 : 0;
-            int middleValue = middle.get() ? 1 : 0;
-            int rightValue = right.get() ? 1 : 0;
-            // compute the single value from the 3 sensors. Notice that the bits
-            // for the outside sensors are flipped depending on left or right
-            // fork. Also the sign of the steering direction is different for left/right.
-            if (goLeft) {
-                binaryValue = leftValue * 4 + middleValue * 2 + rightValue;
-                steeringGain = -des.defaultSteeringGain;
-            } else {
-                binaryValue = rightValue * 4 + middleValue * 2 + leftValue;
-                steeringGain = des.defaultSteeringGain;
-            }
+            getSensorValues();
+            binaryValue = binaryValue(goLeft);
+            steeringGain = goLeft ? -DEFAULT_STEERING_GAIN : DEFAULT_STEERING_GAIN;
 
             // get the default speed and turn rate at this time
             speed = powerProfile[timeInSeconds];
@@ -136,26 +95,18 @@ public class Autonomous {
                     turn = -steeringGain;
             }
             // print current status for debugging
-            if (binaryValue != previousValue) {
-                System.out.println("Time: " + time + " Sensor: " + binaryValue + " speed: " + speed + " turn: " + turn + " atCross: " + atCross);
-            }
+            if (binaryValue != previousValue)
+                printLineStatus();
 
             // set the robot speed and direction
-            // for summer drive train, "normal" wheels
-            des.drive.arcadeDrive(speed, turn);
+            des.drive.arcadeDrive(-speed, -turn);
 
-            // Try this for mecanum drive train:
-            //des.drive.mecanumDrive_Polar(speed, turn, turn);
-
-            if (binaryValue != 0) {
+            if (binaryValue != 0)
                 previousValue = binaryValue;
-            }
-            oldTimeInSeconds = timeInSeconds;
 
             Timer.delay(0.01);
         }
         // Done with loop - stop the robot. Robot ought to be at the end of the line
         des.drive.arcadeDrive(0, 0);
     }
-
 }
