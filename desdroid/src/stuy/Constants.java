6f5ee7564b936bdc8c31c62cacfb2608f40d2689
Kevin Wang <born2run5000@yahoo.com>
2011-01-28 19:00:12 -0500

    Corrected constants.

--- desdroid/src/stuy/Constants.java
+++ desdroid/src/stuy/Constants.java
@@ -1,9 +1,9 @@
 package stuy;
 
 public interface Constants {
-    final int DRIVE_CAN_DEVICE_FRONT_LEFT    = 2;
-    final int DRIVE_CAN_DEVICE_FRONT_RIGHT   = 3;
-    final int DRIVE_CAN_DEVICE_REAR_LEFT     = 4;
+    final int DRIVE_CAN_DEVICE_FRONT_LEFT    = 3;
+    final int DRIVE_CAN_DEVICE_FRONT_RIGHT   = 4;
+    final int DRIVE_CAN_DEVICE_REAR_LEFT     = 2;
     final int DRIVE_CAN_DEVICE_REAR_RIGHT    = 5;
 
     final int ARM_CAN_DEVICE_NUMBER          = 6;
