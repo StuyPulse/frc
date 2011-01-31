b3bc257577ccd1dae161d8b07dc3daeda7bdfcbe
Kevin Wang <born2run5000@yahoo.com>
2011-01-31 16:31:35 -0500

    Added autonomous framework.

--- desdroid/src/stuy/Autonomous.java
+++ desdroid/src/stuy/Autonomous.java
@@ -31,10 +31,58 @@ public class Autonomous implements Constants {
         gyro = new Gyro(GYRO_CHANNEL);
     }
 
+    public void run(int setting) {
+        switch (setting) {
+            case 1:
+                auton1();
+                break;
+            case 2:
+                auton2();
+                break;
+            case 3:
+                auton3();
+                break;
+            case 4:
+                auton4();
+                break;
+        }
+    }
+
     public void lineTrackStraight() {
 
     }
 
+    /**
+     * Follow line straight
+     * Score
+     * Back up along line
+     */
+    public void auton1() {
 
+    }
 
+    /**
+     * Follow line left
+     * Score
+     * Back up along line
+     */
+    public void auton2() {
+
+    }
+
+    /**
+     * Follow line right
+     * Score
+     * Back up along line
+     */
+    public void auton3() {
+
+    }
+
+    /**
+     * Drop uebertube
+     */
+    public void auton4() {
+
+    }
 }
