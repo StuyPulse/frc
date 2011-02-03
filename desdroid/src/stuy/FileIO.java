25452098e204eaa1e76e697db69ae33e4ff52164
Kevin Wang <born2run5000@yahoo.com>
2011-02-03 17:51:08 -0500

    Comments can now be placed into value text files with the '#' character. Also improved documentation in FileIO.java.

--- desdroid/src/stuy/FileIO.java
+++ desdroid/src/stuy/FileIO.java
@@ -13,20 +13,39 @@ import java.io.InputStreamReader;
 import javax.microedition.io.Connector;
 
 /**
- *
+ * Read and parse text files on the cRIO. This is useful for quickly adjusting tuning values using FTP, such as for PID.
+ * Place the '#' character before comments.
  * @author Kevin Wang
  */
 public class FileIO {
 
+    /**
+     * Returns the contents of a file on the root directory of the cRIO, getting rid of comments and trimming whitespace.
+     * @param filename The name of the file to read.
+     * @return The contents of the file.
+     */
     private static String getFileContents(String filename) {
-        String url = "file:///" + filename; // TODO: Make a directory for the text files
+        String url = "file:///values/" + filename; // TODO: Make the values directory and move text files into it
         String contents = "";
         try {
             FileConnection c = (FileConnection) Connector.open(url);
             BufferedReader buf = new BufferedReader(new InputStreamReader(c.openInputStream()));
             String line = "";
+            boolean lineRead = false;
             while ((line = buf.readLine()) != null) {
-                contents += line + "\n";
+                lineRead = false;
+                if (line.charAt(0) != '#') {
+                    for (int i = 0; i < line.toCharArray().length; i++) {
+                        if (line.toCharArray()[i] == '#') {
+                            contents += line.substring(0, i).trim() + "\n";
+                            lineRead = true;
+                            break;
+                        }
+                    }
+                    if (lineRead == false) {
+                        contents += line + "\n";
+                    }
+                }
             }
             c.close();
         }
@@ -36,6 +55,11 @@ public class FileIO {
         return contents;
     }
 
+    /**
+     * Create an array of doubles using values in a text file. Each line in the file corresponds to one element in the array.
+     * @param filename The name of the file to read.
+     * @return The array of values in the file.
+     */
     public static double[] getArray(String filename) {
         String raw = getFileContents(filename);
         StringTokenizer st = new StringTokenizer(raw);
