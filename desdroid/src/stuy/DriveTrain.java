87f0ea6486c2b56aad3f3c2efc11bf63bcb36d50
Blake Elias <blakeelias@gmail.com>
2011-02-04 02:09:28 -0500

    Created scaleInput method for DriveTrain: create a deadband of joystick values that are small enough to be treated as 0 so the robot will not jitter, while still allowing the full range of driver control.

--- desdroid/src/stuy/DriveTrain.java
+++ desdroid/src/stuy/DriveTrain.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package stuy;
 
 import edu.wpi.first.wpilibj.*;
@@ -15,22 +14,27 @@ import java.lang.Math;
  * @author Blake
  */
 public class DriveTrain extends RobotDrive {
+
     int kFrontLeft_val = 0;
     int kFrontRight_val = 1;
     int kRearLeft_val = 2;
     int kRearRight_val = 3;
     int kMaxRPM = 600;
+    /**
+     * Ignore joystick inputs that are less than this number in absolute value.
+     * Scale the rest of the inputs to still allow for the full output range (-1 to 1)
+     */
+    static double minJoystickValue = 0.1;
 
     public DriveTrain(SpeedController frontLeftMotor, SpeedController rearLeftMotor,
             SpeedController frontRightMotor, SpeedController rearRightMotor) {
         super(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
     }
 
-    public void mecanumDrive_Cartesian(double x, double y, double rotation, double gyroAngle)
-    {
-        double xIn = x * Math.abs(x);
-        double yIn = y * Math.abs(y);
-        
+    public void mecanumDrive_Cartesian(double x, double y, double rotation, double gyroAngle) {
+        double xIn = scaleInput(x);
+        double yIn = scaleInput(y);
+
         // Negate y for the joystick.
         yIn = -yIn;
         // Compenstate for gyro angle.
@@ -46,7 +50,7 @@ public class DriveTrain extends RobotDrive {
 
         normalize(wheelSpeeds);
 
-        byte syncGroup = (byte)0x80;
+        byte syncGroup = (byte) 0x80;
 
         m_frontLeftMotor.set(kMaxRPM * wheelSpeeds[kFrontLeft_val] * m_invertedMotors[kFrontLeft_val] * m_maxOutput, syncGroup);
         m_frontRightMotor.set(kMaxRPM * wheelSpeeds[kFrontRight_val] * m_invertedMotors[kFrontRight_val] * m_maxOutput, syncGroup);
@@ -58,9 +62,47 @@ public class DriveTrain extends RobotDrive {
                 CANJaguar.updateSyncGroup(syncGroup);
             } catch (CANNotInitializedException e) {
                 m_isCANInitialized = false;
-            } catch (CANTimeoutException e) {}
+            } catch (CANTimeoutException e) {
+            }
         }
 
-        if (m_safetyHelper != null) m_safetyHelper.feed();
+        if (m_safetyHelper != null) {
+            m_safetyHelper.feed();
+        }
+    }
+
+    /**
+     * Modify a joystick input to round any super-small values to 0.  Should prevent
+     * robot from jittering when the joysticks have a small default value.
+     *
+     * Though small values are pulled down to 0, inputs higher than the specified
+     * threshold will still cover the entire range of outputs, though the rate
+     * at which the output grows is slightly steeper.  This change should be small
+     * enough to still allow for good manual control.
+     *
+     * The graph of scaleInput's value (vertical axis) versus original joystick
+     * input (horizontal axis) looks like this:
+     *
+     *                /
+     *               /
+     *   -1   _ _ _ /  1
+     *       /
+     *      /
+     *     /
+     *
+     * @param x A joystick value from -1 to 1.
+     * @return Close to the original input, but scaled to make small values return
+     * 0 while still allowing the full range of outputs, -1 to 1.
+     */
+    private static double scaleInput(double x) {
+        if (Math.abs(x) < minJoystickValue) {
+            return 0;
+        }
+
+        double scaledSlope = 1 / (1 - minJoystickValue);
+        if (x > 0) {
+            return (x - minJoystickValue) * scaledSlope;
+        }
+        return (x + minJoystickValue) * scaledSlope;
     }
 }
