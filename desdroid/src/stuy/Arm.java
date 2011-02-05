5cbbfe6411478bb45b8216fecd69722d10fce5fb
Kevin Wang <born2run5000@yahoo.com>
2011-02-05 11:18:51 -0500

    Continued working on the Arm class. Made it so that the motor has to run full power in each direction.

--- desdroid/src/stuy/Arm.java
+++ desdroid/src/stuy/Arm.java
@@ -23,13 +23,7 @@ public class Arm implements Constants {
         des = d;
 
         try {
-            armMotor = new CANJaguar(ARM_CAN_DEVICE_NUMBER, CANJaguar.ControlMode.kPosition);
-            armMotor.setPositionReference(CANJaguar.PositionReference.kPotentiometer);
-            armMotor.setPID(ARM_P, ARM_I, ARM_D);
-
-            armMotor.configSoftPositionLimits(LOWER_ARM_POT_LIM, UPPER_ARM_POT_LIM); //TODO: FIND THESE LOWER, UPPER POT LIMITS
-
-            armMotor.enableControl();
+            armMotor = new CANJaguar(ARM_CAN_DEVICE_NUMBER, CANJaguar.ControlMode.kPercentVbus);
         }
         catch (Exception e) {
             des.oi.setStuffsBrokenLED(true);
@@ -37,12 +31,20 @@ public class Arm implements Constants {
     }
 
     /**
-     * Rotate the arm manually.
+     * Rotate the arm manually. Arm motor must be run at full speed.
      * @param speed Speed to rotate the arm. (-1.0 to 1.0)
      */
     public void rotate(double speed) {
         try {
-            armMotor.setX(speed);
+            if (speed >= 0.5) {
+                armMotor.setX(1);
+            }
+            else if (speed <= -0.5) {
+                armMotor.setX(-1);
+            }
+            else {
+                armMotor.setX(0);
+            }
         }
         catch (Exception e) {
             des.oi.setStuffsBrokenLED(true);
@@ -54,10 +56,39 @@ public class Arm implements Constants {
      * @param height The height to set the arm to.
      */
     public void setHeight(double height) {
+        double init = Timer.getFPGATimestamp();
         try {
+            toPositionControl();
+            
+            armMotor.enableControl();
             armMotor.setX(height);
+
+            armMotor.disableControl();
+
+            toPercentVbusControl();
         }
         catch(Exception e) {
+            des.oi.setStuffsBrokenLED(true);
+        }
+    }
+
+    public void toPositionControl() {
+        try {
+            armMotor.changeControlMode(CANJaguar.ControlMode.kPosition);
+            armMotor.setPositionReference(CANJaguar.PositionReference.kPotentiometer);
+            armMotor.setPID(ARM_P, ARM_I, ARM_D);
+            armMotor.configSoftPositionLimits(LOWER_ARM_POT_LIM, UPPER_ARM_POT_LIM);
+        }
+        catch (Exception e) {
+            des.oi.setStuffsBrokenLED(true);
+        }
+    }
+
+    public void toPercentVbusControl() {
+        try {
+            armMotor.changeControlMode(CANJaguar.ControlMode.kPercentVbus);
+        }
+        catch (Exception e) {
             des.oi.setStuffsBrokenLED(true);
         }
     }
