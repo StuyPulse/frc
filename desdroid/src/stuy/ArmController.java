3dc9ce7af57dbc23678a2a6ae0cf0aae501f934b
Blake Elias <blakeelias@gmail.com>
2011-03-01 18:22:07 -0500

    Added documentation to ArmController.java

--- desdroid/src/stuy/ArmController.java
+++ desdroid/src/stuy/ArmController.java
@@ -16,6 +16,16 @@ public class ArmController extends Thread implements Constants {
     double setpoint;
     DESdroid des;
 
+    /**
+     * Create a new ArmController to set the arm's position to a setpoint.  Uses
+     * potentiometer feedback to reach the correct position.  Runs in its own
+     * thread so that we can drive the robot at the same time as the arm.
+     * @param d A reference to the DESdroid object where the robot is running.
+     * @param buttonNum The button number corresponding to the desired height
+     * (one of Constants.X_Y_BUTTON, with X and Y being SIDE, MIDDLE, etc.
+     * @param trimAmount The amount of adjustment to apply to the setpoint,
+     * can be provided by driver input from the OI.  0 is no adjustment.
+     */
     public ArmController(DESdroid d, int buttonNum, double trimAmount) {
         active = true;
         des = d;
@@ -47,17 +57,28 @@ public class ArmController extends Thread implements Constants {
         }
     }
 
+    /**
+     * Run the position control loop (in its own thread).
+     */
     public void run() {
         while (active) {
             double currentVal = des.arm.getPosition();
-            if (currentVal - setpoint > 0.005) {       // Intentionally does not depend on a software limit, because there is a physical limit.
+            // Check if the arm is above/below the setpoint, with a small tolerance.
+            if (currentVal - setpoint > 0.005) {       // Intentionally does not depend on a software limit, because there is a physical limit
+                // Move up
                 des.arm.armMotor.set(1);
-            } else if (currentVal - setpoint < -0.005 && currentVal < Arm.UPPER_ARM_POT_LIM) {  
+            } else if (currentVal - setpoint < -0.005 && currentVal < Arm.UPPER_ARM_POT_LIM) {
+                // Move down
                 des.arm.armMotor.set(-1);
             } else {
+                // We're done!  Stop the motor and end the thread.
                 des.arm.armMotor.set(0);
                 this.end();
             }
+
+            // Pulse the motor on and off at different frequencies depending on
+            // how far we have to move.  For big amounts of error, run the motor
+            // longer than it is off, for small error run it less frequently.
             double delayVal = Arm.MAX_ARM_DELAY * Math.abs(des.arm.getPosition() - setpoint);
             Timer.delay(delayVal);
             des.arm.armMotor.set(0);
