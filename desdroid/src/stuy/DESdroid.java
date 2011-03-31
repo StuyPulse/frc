29e4b807f4f09256673be8bc71727e1788698060
Kevin Wang <born2run5000@yahoo.com>
2011-03-31 15:30:40 -0400

    Raised minibot mode speed from 1/4 to 5/16.

--- desdroid/src/stuy/DESdroid.java
+++ desdroid/src/stuy/DESdroid.java
@@ -132,9 +132,9 @@ public class DESdroid extends SimpleRobot implements Constants {
 
             if (minibotMode) {
                 drive.mecanumDrive_Cartesian(
-                        -leftStick.getX() * 0.25, // X translation (horizontal strafe)
-                        -leftStick.getY() * 0.25, // Y translation (straight forward)
-                        rightStick.getX() * 0.25, // rotation (getX() > 0 is clockwise)
+                        -leftStick.getX() * 0.3125, // X translation (horizontal strafe)
+                        -leftStick.getY() * 0.3125, // Y translation (straight forward)
+                        rightStick.getX() * 0.3125, // rotation (getX() > 0 is clockwise)
                         0, // use gyro for field-oriented drive
                         true);            // deadband the inputs?
             }
